e822b57a1fb952eba7ffa0285d04cec7277feaa7
ayoubsmayen2 <ayoub.smayen@esprit.tn>
2021-06-14 21:09:56 +0200

    adder

---
+++ build/precache-manifest.a2f46b4ee775d1f9ff48f2761d2b5a34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db71b632ed82a5f39fd71dcb55953311",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44a6eb22c0616cc5aab8",
+    "url": "/static/css/main.3c825a5b.chunk.css"
+  },
+  {
+    "revision": "df0baa6e6211d42a710b",
+    "url": "/static/js/2.05422bb0.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/static/js/2.05422bb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44a6eb22c0616cc5aab8",
+    "url": "/static/js/main.2d747e40.chunk.js"
+  },
+  {
+    "revision": "559ea9b268b7819359f3",
+    "url": "/static/js/runtime-main.786b8bbc.js"
+  },
+  {
+    "revision": "feda678008a3da9d35bca2f0e4444e2f",
+    "url": "/static/media/logo.feda6780.svg"
+  }
+]);
